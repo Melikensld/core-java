72236efc432a9f3e1d6e6261678570bf77c5fcd2
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-23 16:37:18 +0300

    Task 10

---
+++ src/Task10.java
@@ -0,0 +1,8 @@
+public class Task10 {
+    public static void main(String[] args) {
+        int[] nums = {15, 25, 35, 45, 55};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
