205a10a4641ac022fb29c2547594900e18e3ceab
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-23 15:11:23 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        // Task2
+        int age = 25;
+        double height = 1.75;
+        float weight = 68.5f;
+        boolean isStudent = true;
+        char gender = 'F';
+        String name = "Melike";
+
+        final double PI = 3.14159;
+
+        System.out.println("İsim: " + name);
+        System.out.println("Yaş: " + age);
+        System.out.println("Boy: " + height + " metre");
+        System.out.println("Kilo: " + weight + " kg");
+        System.out.println("Öğrenci mi?: " + isStudent);
+        System.out.println("Cinsiyet: " + gender);
+        System.out.println("Pi sayısı: " + PI);
     }
 }
