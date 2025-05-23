c6c0d74c40e9081c8a7d5723a7102a3ea21a6198
Melike Ünsaldı <melikeunsaldi09@gmail.com>
2025-05-23 16:01:37 +0300

    Task 4

---
+++ src/Task4.java
@@ -0,0 +1,11 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int a = 350;
+        int b = 25;
+
+        System.out.println("toplama: " +(a+b));
+        System.out.println("çıkarma: " +(a-b));
+        System.out.println("çarpma: " +(a*b));
+        System.out.println("bölme: " +(a/b));
+    }
+}
